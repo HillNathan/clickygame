9a44c25c58ac852c40f4042e78628bdf1ca6d107
Nathan Hill <pjsstp@gmail.com>
2019-09-30 20:13:50 -0400

    Updates

---
+++ precache-manifest.2946c3818ad682c578ea4d4e612a52d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0987166939856507c5bddb19e4f3884b",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "4c098f09868f6c2ba5ea",
+    "url": "/clickygame/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "f14dba4c318e7d746438",
+    "url": "/clickygame/static/js/2.f3a353a5.chunk.js"
+  },
+  {
+    "revision": "4c098f09868f6c2ba5ea",
+    "url": "/clickygame/static/js/main.34ee5c20.chunk.js"
+  },
+  {
+    "revision": "38b39f416022f75e4595",
+    "url": "/clickygame/static/js/runtime-main.dd7f9e86.js"
+  }
+]);
